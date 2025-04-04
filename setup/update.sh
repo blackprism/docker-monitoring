3afcc6464d521917199bc9535650e44ba933bf0f
srm <srm@bouh.org>
2025-04-04 14:22:59 +0200

    feat: increase query length for loki and tempo

--- setup/update.sh
+++ setup/update.sh
@@ -3,12 +3,12 @@
 printf "%s" "Grafana admin password: "
 read -sr admin_password
 
-docker run --rm --add-host host.docker.internal:host-gateway -v "${PWD}/setup/grafana:/grafana" alpine/curl -s "http://host.docker.internal:3000/api/datasources" -u "admin:${admin_password}" -H 'Content-Type: application/json' --data-binary "@/grafana/datasource/prometheus.json"
+docker run --rm --network docker-monitoring_default -v "${PWD}/setup/grafana:/grafana" alpine/curl -s "http://grafana:3000/api/datasources" -u "admin:${admin_password}" -H 'Content-Type: application/json' --data-binary "@/grafana/datasource/prometheus.json"
 
-docker run --rm --add-host host.docker.internal:host-gateway -v "${PWD}/setup/grafana:/grafana" alpine/curl -s "http://host.docker.internal:3000/api/datasources" -u "admin:${admin_password}" -H 'Content-Type: application/json' --data-binary "@/grafana/datasource/pyroscope.json"
+docker run --rm --network docker-monitoring_default -v "${PWD}/setup/grafana:/grafana" alpine/curl -s "http://grafana:3000/api/datasources" -u "admin:${admin_password}" -H 'Content-Type: application/json' --data-binary "@/grafana/datasource/pyroscope.json"
 
-docker run --rm --add-host host.docker.internal:host-gateway -v "${PWD}/setup/grafana:/grafana" alpine/curl -s "http://host.docker.internal:3000/api/datasources" -u "admin:${admin_password}" -H 'Content-Type: application/json' --data-binary "@/grafana/datasource/loki.json"
+docker run --rm --network docker-monitoring_default -v "${PWD}/setup/grafana:/grafana" alpine/curl -s "http://grafana:3000/api/datasources" -u "admin:${admin_password}" -H 'Content-Type: application/json' --data-binary "@/grafana/datasource/loki.json"
 
-docker run --rm --add-host host.docker.internal:host-gateway -v "${PWD}/setup/grafana:/grafana" alpine/curl -s "http://host.docker.internal:3000/api/datasources" -u "admin:${admin_password}" -H 'Content-Type: application/json' --data-binary "@/grafana/datasource/tempo.json"
+docker run --rm --network docker-monitoring_default -v "${PWD}/setup/grafana:/grafana" alpine/curl -s "http://grafana:3000/api/datasources" -u "admin:${admin_password}" -H 'Content-Type: application/json' --data-binary "@/grafana/datasource/tempo.json"
 
-docker run --rm --add-host host.docker.internal:host-gateway -v "${PWD}/setup/grafana:/grafana" alpine/curl -s "http://host.docker.internal:3000/api/dashboards/db" -u "admin:${admin_password}" -H 'Content-Type: application/json' --data-binary "@/grafana/dashboard/docker.json"
+docker run --rm --network docker-monitoring_default -v "${PWD}/setup/grafana:/grafana" alpine/curl -s "http://grafana:3000/api/dashboards/db" -u "admin:${admin_password}" -H 'Content-Type: application/json' --data-binary "@/grafana/dashboard/docker.json"
